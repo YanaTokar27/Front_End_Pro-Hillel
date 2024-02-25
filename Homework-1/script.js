6b5bfe11d597994c617f38f11277801af39d0dab
yanatokar <yanochka.tokar22@gmail.com>
2024-02-25 17:22:24 +0200

    Homework-1

---
+++ Homework-1/script.js
@@ -0,0 +1,23 @@
+const action = prompt('Яку дію Ви хочете зробити + - * / ?');
+const firstNum = +prompt('Введіть перше число');
+const secondNum = +prompt('Введіть друге число');
+const mathAction = `${firstNum} ${action} ${secondNum} = `;
+
+
+
+if (action === '+') {
+    const add = firstNum + secondNum;
+    // alert(mathAction + ' = ' + add)
+    alert(mathAction + add)
+} else if (action === '-') {
+    const sub = firstNum - secondNum;
+    alert(mathAction + sub)
+} else if (action === '*') {
+    const mult = firstNum * secondNum;
+    alert(mathAction + mult)
+} else if (action === '/') {
+    const div = firstNum / secondNum;
+    alert(mathAction + div)
+} else {
+    alert('Ви ввели недопустимі значення')
+}
