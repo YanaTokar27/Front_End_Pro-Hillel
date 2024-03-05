378bb6761f41c2237df7a02d044d40d12490ba56
yanatokar <yanochka.tokar22@gmail.com>
2024-03-05 19:16:13 +0200

    Lesson-5

--- Homework-4/script.js
+++ Homework-4/script.js
@@ -1,115 +1,122 @@
 const task1 = () => {
-    let str = '';
+    let string = '';
 
-    for (let i = 20; i <= 30; i++) {
-        str += i < 30 ? `${i}, ` : `${i}`;
+    for (let i = 20; i < 30; i += 0.5) {
+        string += i < 29.5 ? `${i}, ` : `${i}`;
     }
-    console.log(str);
+    console.log(string);
 }
 
+const task2v1 = () => {                  //варіант-1
+    let table = {};
 
-console.log(123);
+    for (let i = 10; i <= 100; i += 10) {
+        for (let k = 27; k <= 100; k += 27) {
+            if (!table[i]) {
+                table[i] = [];
+                table[i].push(i * k);
+            }
+        }
+    }
+    console.log(table);
+}
 
-const task2 = () => {
+const task2v2 = () => {                  //варіант-2
+    const oneUSD = 27;
+    let doll = 10;
 
+    do {
+        const cost = oneUSD * doll;
+        console.log(doll + ' USD = ' + cost + ' грн');
+        doll += 10;
+    } while (doll <= 100)
 }
 
 const task3 = () => {
+    const number = +prompt('Введіть число 1-100');
 
+    if (number && number <= 100) {
+        for (let i = 1; i * i <= number; i++) {
+            console.log(i);
+        }
+    } else {
+        alert('Значення введене некоректно');
+    }
 }
 
 const task4 = () => {
+    const a = +prompt('Введіть число');
+    let isPrime = true;
 
+    for (let i = 2; i < a; i++) {
+        if (a % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(a + ' просте число');
+    } else {
+        console.log(a + ' непросте число');
+    }
 }
 
 const task5 = () => {
+    const a = +prompt('Введіть число');
+    let isSquar = true;
+    let curr = a;
+
+    // for (let i = 3; i < a; i++) {
+    //     if (a / 3 === 0) {
+    //         isSquar = false;
+    //         break;
+    //     }
+    // }
+
+    if (a) {
+        while (curr > 1) {
+            if (curr % 3 !== 0) {
+                isSquar = false;
+                break;
+            }
+            curr /= 3;
+        }
+
+        if (isSquar) {
+            console.log(a + ' - є результатом зведення числа 3 у степінь');
+        } else {
+            console.log(a + ' - не є результатом зведення числа 3 у степінь');
+        }
+    } else {
+        console.log('Введіть коректне число');
+    }
 
 }
 
-const task6 = () => {
+// Дане деяке число
+let targetNumber = +prompt('enter number');
 
+// Починаємо з базового числа (3 в нульовій ступені)
+let currentNumber = 3;
+
+// Цикл для підняття числа 3 до ступеня, поки воно не перевищить задане число
+while (currentNumber < targetNumber) {
+    currentNumber *= 3; // Піднімаємо число 3 до наступного ступеня
 }
 
-const task7 = () => {
-
-}
-
-const task8 = () => {
-
-}
-
-const task9 = () => {
-
-}
-
-const task10 = () => {
-
-}
-
-const task11 = () => {
-
-}
-
-const task12 = () => {
-
-}
-
-const task13 = () => {
-
-}
-
-const task14 = () => {
-
-}
-
-const task15 = () => {
-
-}
-
-const task16 = () => {
-
-}
-
-const task17 = () => {
-
-}
-
-const task18 = () => {
-
-}
-
-const task19 = () => {
-
-}
-
-const task20 = () => {
-
-}
-
-const task21 = () => {
-
+// Перевірка, чи задане число можна отримати
+if (currentNumber === targetNumber) {
+    console.log(targetNumber + ' можна отримати, зведене до числа 3 у деякий ступінь.');
+} else {
+    console.log(targetNumber + ' не можна отримати, зведене до числа 3 у деякий ступінь.');
 }
 
 
+// task1();
+// task2v1();
+// task2v2();
+// task3();
+// task4();
+// task5();
 
-task1();
-task2();
-task3();
-task4();
-task5();
-task6();
-task7();
-task8();
-task9();
-task10();
-task11();
-task12();
-task13();
-task14();
-task15();
-task16();
-task17();
-task18();
-task19();
-task20();
-task21();
