729de84f73658179755f007e1a0d38d1f7017ef6
yanatokar <yanochka.tokar22@gmail.com>
2024-03-17 14:15:38 +0200

    Homework-8

---
+++ Homework-8/script.js
@@ -0,0 +1,18 @@
+const task1 = () => {
+    function pow(num, degree) {
+
+        return degree > 1 ? num * pow(num, degree - 1) : num
+
+        // if (degree > 1) {
+        //     return num *= pow(num, degree - 1)
+        // }
+
+        // return num
+    }
+
+    console.log(pow(8, 2));
+}
+
+task1();
+
+
