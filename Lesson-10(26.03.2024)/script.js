b7f75b060ba97a464fa87dc8101ef9b855c5b7b8
yanatokar <yanochka.tokar22@gmail.com>
2024-04-01 16:15:21 +0300

    Lesson-10

--- Lesson-10(26.03.2024)/script.js
+++ Lesson-10(26.03.2024)/script.js
@@ -78,57 +78,7 @@ const task8 = () => {       //scroll - up
     })
 }
 
-const task9 = () => {
 
-}
-
-const task10 = () => {
-
-}
-
-const task11 = () => {
-
-}
-
-const task12 = () => {
-
-}
-
-const task13 = () => {
-
-}
-
-const task14 = () => {
-
-}
-
-const task15 = () => {
-
-}
-
-const task16 = () => {
-
-}
-
-const task17 = () => {
-
-}
-
-const task18 = () => {
-
-}
-
-const task19 = () => {
-
-}
-
-const task20 = () => {
-
-}
-
-const task21 = () => {
-
-}
 
 
 
@@ -136,20 +86,7 @@ const task21 = () => {
 // task2();
 // task3();
 // task4();
-// task5();
+task5();
 // task6();
 // task7();
 // task8();
-task9();
-task10();
-task11();
-task12();
-task13();
-task14();
-// task15();
-// task16();
-// task17();
-// task18();
-// task19();
-// task20();
-// task21();
