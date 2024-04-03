63e40f31edbf18f9610debcfa6ac46e8fca57dde
yanatokar <yanochka.tokar22@gmail.com>
2024-04-03 22:39:34 +0300

    Homework-14

--- Homework-14/script.js
+++ Homework-14/script.js
@@ -1,16 +1,11 @@
 const buy = document.querySelector('.card_buy');
 const dataBuyer = document.querySelector('.data_buyer');
-const submit = document.querySelector('.confirm');
 const inform_buyer = document.querySelector('.inform_buyer');
-const city = document.querySelector('.city');
-
+const infoOrder = document.querySelector('.infoOrder');
 
 buy.addEventListener('click', function () {
     dataBuyer.classList.remove('hiden');
 })
-
-
-
 
 inform_buyer.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -25,25 +20,51 @@ inform_buyer.addEventListener('submit', function (event) {
             formObject[key] = value;
         }
     })
-    // console.log(formObject);
-    validateInput(formObject);
+
+    if (validateInput(formObject)) {
+        infoOrder.classList.remove('hiden')
+        printInputs(formObject);
+    }
 })
 
 function validateInput(object) {
-
+    let result = true;
     for (const [key, value] of Object.entries(object)) {
-        console.log(`${key}: ${value}`);
         const inputElement = document.querySelector(`.${key}`);
+
         if (!inputElement) {
             continue
         }
         if (!value) {
             inputElement.classList.add('error');
+            result = false
         } else {
             inputElement.classList.remove('error');
         }
     }
+    return result
 }
+
+function printInputs(object) {
+    infoOrder.textContent = '';
+
+    addTextToInfoOrder(infoOrder, `ПІБ покупця: ${object.buyer}`);
+    addTextToInfoOrder(infoOrder, `Місто: ${object.city}`);
+    addTextToInfoOrder(infoOrder, `Склад Нової Пошти: ${object.number_post}`);
+    addTextToInfoOrder(infoOrder, `Вид оплати: ${object.payment}`);
+    addTextToInfoOrder(infoOrder, `Кількість товару: ${object.quantity}`);
+
+    if (object.comment) {
+        addTextToInfoOrder(infoOrder, `Коментар до замовлення: ${object.comment}`);
+    }
+}
+
+function addTextToInfoOrder(infoOrder, text) {
+    const p = document.createElement('p');
+    p.textContent = text;
+    infoOrder.appendChild(p);
+}
+
 
 
 
