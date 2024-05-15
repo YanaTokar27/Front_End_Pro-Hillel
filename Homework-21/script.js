db371eb2179dac80719c55c46504b0ba26961d9e
yanatokar <yanochka.tokar22@gmail.com>
2024-05-15 12:56:58 +0300

    Homework-21

--- Homework-21/script.js
+++ Homework-21/script.js
@@ -1,14 +1,14 @@
-function* Foo() {
-    console.log('start')
-    yield 1
-    yield 2
-    yield 3
-    console.log('finish')
-}
+// function* Foo() {
+//     console.log('start')
+//     yield 1
+//     yield 2
+//     yield 3
+//     console.log('finish')
+// }
 
-const iterator1 = Foo()
-console.log(iterator1.next().value) // 1
-console.log(iterator1.next().done) // false
+// const iterator1 = Foo()
+// console.log(iterator1.next().value) // 1
+// console.log(iterator1.next().done) // false
 
 
 function Boo() {
@@ -16,22 +16,23 @@ function Boo() {
     const values = [1, 2, 3];
 
     console.log('start');
-
     return {
-        next: function () {
+        next: () => {
             if (index < values.length) {
                 return { value: values[index++], done: false };
             } else {
                 console.log('finish');
-                return { done: true };
+                return ({ values: undefined, done: true });
             }
         }
     };
 }
 
 const iterator = Boo();
-
+//Start
 console.log(iterator.next().value); // 1
-console.log(iterator.next().done); // false
-
+console.log(iterator.next().value); // 2
+console.log(iterator.next().value); // 3
+//Finish
+console.log(iterator.next()); // true
 
