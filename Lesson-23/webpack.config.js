24f10badd6fd3a4d5e5d4e5df4e3fd6f35b8600a
yanatokar <yanochka.tokar22@gmail.com>
2024-05-16 16:35:58 +0300

    Lesson-23

--- Lesson-23/webpack.config.js
+++ Lesson-23/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -7,5 +8,21 @@ module.exports = {
         filename: 'build.js',
         path: path.resolve(__dirname, 'build'),
     },
-    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
 };
