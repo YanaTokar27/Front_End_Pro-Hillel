08707286e821b9b6ce586cd9c981c54637952363
yanatokar <yanochka.tokar22@gmail.com>
2024-05-22 12:07:33 +0300

    Homework-22

---
+++ Homework-22/src/index.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+import { getButton } from "./button/button";
+import './index.css';
+import image1 from './assets/fox.jpeg';
+import image2 from './assets/rabbit.jpeg';
+
+
+getButton();
+
+const picture = document.createElement('img');
+picture.setAttribute('src', image1);
+document.querySelector('body').appendChild(picture);
+
+const picture2 = document.createElement('img');
+picture2.setAttribute('src', image2);
+picture2.setAttribute('width', '200');
+picture2.setAttribute('height', '300');
+
+document.querySelector('body').appendChild(picture2);
+
