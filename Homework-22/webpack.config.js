06d6d1aa216c861e04fc72246a9141e3300dfc5a
yanatokar <yanochka.tokar22@gmail.com>
2024-05-22 22:37:36 +0300

    Homework-22

--- Homework-22/webpack.config.js
+++ Homework-22/webpack.config.js
@@ -5,7 +5,7 @@ const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    // mode: 'development',
     output: {
         filename: 'build.js',
         path: path.resolve(__dirname, 'build'),
