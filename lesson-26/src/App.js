234325a5ae034c34632b04f48e8829e95a5e4c3b
yanatokar <yanochka.tokar22@gmail.com>
2024-05-26 20:10:27 +0300

    Lesson-26

--- lesson-26/src/App.js
+++ lesson-26/src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input/input';
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* {formatName(user)} */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Input />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ lesson-26/src/components/Input/input.js
@@ -0,0 +1,8 @@
+const Input = () => {
+    return <label>
+        Name:
+        <input type="text"></input>
+    </label>
+}
+
+export default Input;
