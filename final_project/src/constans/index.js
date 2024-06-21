3044225c2b257cb2b64259945190cb2bf4c937d9
yanatokar <yanochka.tokar22@gmail.com>
2024-06-21 14:35:50 +0300

    API_URL

---
+++ final_project/src/constans/index.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://6628f7e654afcabd0737a2a7.mockapi.io/';
--- final_project/src/pages/Products/Products.js
+++ final_project/src/pages/Products/Products.js
@@ -1,37 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import './Products.css';
-import Table from '../../components/Table/Table';
-import LogoWhite from '../../assets/logo_white.svg'
-import ButtonProducts from '../../components/ButtonProducts/ButtonProducts';
+import React, { useState, useEffect } from "react";
+import "./Products.css";
+import Table from "../../components/Table/Table";
+import LogoWhite from "../../assets/logo_white.svg";
+import ButtonProducts from "../../components/ButtonProducts/ButtonProducts";
 import { FaRegUser } from "react-icons/fa6";
 import { GoPlus } from "react-icons/go";
+import { API_URL } from "../../constans";
 
 function Products() {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        const getProducts = async () => {
-            const response = await fetch(
-                'https://6628f7e654afcabd0737a2a7.mockapi.io/products');
-            const data = await response.json();
-            setProducts(data);
-            console.log("Products loaded");
-        }
-        getProducts();
-    }, []);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(`${API_URL}/products`);
+      const data = await response.json();
+      setProducts(data);
+      console.log("Products loaded");
+    };
+    getProducts();
+  }, []);
 
-    return (
-        <div className='Products'>
-            <div className='LogoWhite' >
-                <img src={LogoWhite} alt="logoGreen" />
-            </div >
-            <div className='Buttons'>
-                <ButtonProducts icon={<FaRegUser className='img user' />} label="Preview" />
-                <ButtonProducts icon={<GoPlus className='img plus' />} label="Add product" />
-            </div>
-            <h2 className='Title'>Products</h2>
-            <Table products={products} />
-        </div>
-    )
+  return (
+    <div className="Products">
+      <div className="LogoWhite">
+        <img src={LogoWhite} alt="logoGreen" />
+      </div>
+      <div className="Buttons">
+        <ButtonProducts
+          icon={<FaRegUser className="img user" />}
+          label="Preview"
+        />
+        <ButtonProducts
+          icon={<GoPlus className="img plus" />}
+          label="Add product"
+        />
+      </div>
+      <h2 className="Title">Products</h2>
+      <Table products={products} />
+    </div>
+  );
 }
 
 export default Products;
